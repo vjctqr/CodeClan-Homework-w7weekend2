0eee26216698be2b5d23237aa6f65fbf098135f2
Victor <victorsetlhare@gmail.com>
2020-05-17 13:58:15 +0100

    The List Component import scripts

--- src/components/MealsList.vue
+++ src/components/MealsList.vue
@@ -1,11 +1,18 @@
 <template>
-  
+  <div>
+      <the-list-component v-for="(meal, index) in meals" :meals="meal" :key="index"></the-list-component>
+  </div>
 </template>
 
 <script>
+import TheListComponent from './TheListComponent.vue'
+
 export default {
     name: 'meals-list',
     props: ['meals'],
+    components:{
+        "the-list-component": TheListComponent
+    }
 
 }
 </script>
