ad634535c14b632765b1b573f6692e376a1e5340
Victor <victorsetlhare@gmail.com>
2020-05-17 13:36:59 +0100

    The List Component create

---
+++ src/components/TheListComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+import {eventBus} from '../main.js'
+
+export default {
+    name: 'the-list-component',
+    props: ['meals'],
+
+}
+</script>
+
+<style>
+
+</style>
